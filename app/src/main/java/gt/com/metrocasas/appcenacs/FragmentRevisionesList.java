bc120391be42dc7d906fb62757e569bb31dac4cd
Marco Barrios <mbarrios@grupodevisa.com>
2016-07-29 09:40:49 -0600

    Correcciones

--- app/src/main/java/gt/com/metrocasas/appcenacs/FragmentRevisionesList.java
+++ app/src/main/java/gt/com/metrocasas/appcenacs/FragmentRevisionesList.java
@@ -8,7 +8,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/gt/com/metrocasas/appcenacs/MainActivity.java
+++ app/src/main/java/gt/com/metrocasas/appcenacs/MainActivity.java
@@ -18,11 +18,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
